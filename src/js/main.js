2d23590ab086021d8a2a6f4937c6817623f37890
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-01-06 09:38:39 +0100

    Paint favourite

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 
 // QUERY SELECTOR
 const charactersUl = document.querySelector(".js_charactersUl");
+const favouriteCharactersUl = document.querySelector( ".js_favouriteCharactersUl"
+);
 
 const btnSearch = document.querySelector(".js_btnSearch");
 // const userInput = document.querySelector('.js_userInput');
@@ -9,12 +11,13 @@ const btnSearch = document.querySelector(".js_btnSearch");
 //VARIABLES DE DATOS
 
 let charactersData = [];
+// let favouriteData = [];
 
 // FUNCIONES
 
 function showOne(characterObj) {
   charactersUl.innerHTML += ` 
-        <li class="characterCard js_mainList" >
+        <li class="characterCard js_mainList" data-id="${characterObj._id}" >
           <img
           src= ${characterObj.imageUrl}
             
@@ -25,31 +28,43 @@ function showOne(characterObj) {
         `;
 }
 
-
-
 function showAll() {
   for (const eachCharacter of charactersData) {
     showOne(eachCharacter);
   }
+
   const allCharactersLi = document.querySelectorAll(".js_mainList");
   console.log(allCharactersLi);
-//como es una lista de arrays, no le puedes poner directamente el evento a const allCharacterLi
+  //como es una lista de arrays, no le puedes poner directamente el evento a const allCharacterLi
 
   for (const characterLi of allCharactersLi) {
-    characterLi.addEventListener ('click', handleClickCharacter);
-
+    characterLi.addEventListener("click", handleClickCharacter);
   }
 }
 
-
-
 //FUNCIONES DE EVENTOS (Handle..)
 
-function handleClickCharacter(event){
+function handleClickCharacter(event) {
   const clickedCharacterLi = event.currentTarget;
   console.log(clickedCharacterLi);
 
-  clickedCharacterLi.classList.toggle('favouriteCard');
+  clickedCharacterLi.classList.toggle("favouriteCard");
+
+  console.log(clickedCharacterLi.dataset.id);
+
+  const selectedCharacterObj = charactersData.find((eachCharacter) => eachCharacter.id === clickedCharacterLi.dataset._id);
+  console.log(selectedCharacterObj);
+
+         favouriteCharactersUl.innerHTML += `
+        <li class= "favouriteCharacter_Card js_mainList" >
+         <img
+         src= ${selectedCharacterObj.imageUrl}
+
+          alt=""
+     />
+        <h4>${selectedCharacterObj.name}</h4>
+       </li>
+  `;
 }
 // function handleClickSearch(){
 // charactersUl.innerHTML=userInput.value;
@@ -57,10 +72,10 @@ function handleClickCharacter(event){
 // }
 
 // EVENTOS
-btnSearch.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("Holis");
-});
+// btnSearch.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   console.log("Holis");
+// });
 // CODIGO CUANDO CARGA LA PAGINA
 
 fetch("//api.disneyapi.dev/character?pageSize=50")
